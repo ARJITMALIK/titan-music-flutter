ff13eefaf480c7149acce64f370d46e89aa765c8
ARJITMALIK <arjitmalik2001@gmail.com>
2024-02-24 15:24:37 +0530

    initial commit

---
+++ lib/pages/ExplorePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ExplorePage extends StatelessWidget {
+  const ExplorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Explore Music"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[Text("explore page")],
+        ),
+      ),
+    );
+  }
+}
